a8d34c00193042ee4e725b0e71ac3f0fed228d30
unknown <Stephen@foxdeploy.com>
2020-01-20 13:18:13 -0500

    adding childrens videos

---
+++ Fashion-WordCloud.ps1
@@ -0,0 +1,19 @@
+﻿#C:\Users\Stephen\Dropbox\Code\Powershell\PSYouTubeScrapes
+
+$girlsFashion = get-content ".\1-9-2020-11_31_44AM-InfoOutput.json" | ConvertFrom-Json
+$tags = $girlsFashion.Tags | select Tags
+
+$mostPopularTags = $tags | Group-Object | Sort-Object Count -Descending | select -First 50
+
+$tags | ? $PSItem -in $mostPopularTags.Name
+
+$tagCount = New-Object System.Collections.ArrayList
+$mostPopularTags | % {
+    $name = $_.Name 
+    "adding $name"
+    0..$_.Count | % {
+        $tagCount.Add($Name)
+    }
+}
+
+$tagCount  | New-WordCloud -Path ..\GirlsFashion1.svg -FocusWord "GirlsFashion" 
---
+++ WeightLoss-WordCloud.ps1
@@ -0,0 +1,21 @@
+﻿#C:\Users\Stephen\Dropbox\Code\Powershell\PSYouTubeScrapes
+
+$girlsFashion = get-content ".\1-9-2020-12_12_23PM-InfoOutput.json" | ConvertFrom-Json
+
+$girlsFashion[0]
+$tags = $girlsFashion | select Tags
+
+$mostPopularTags = $tags.Tags | Group-Object | Sort-Object Count -Descending | select -First 55
+
+#$tags | ? Tags -in $mostPopularTags.Name
+
+$tagCount = New-Object System.Collections.ArrayList
+$mostPopularTags | % {
+    $name = $_.Name 
+    "adding $name"
+    0..$_.Count | % {
+        $tagCount.Add($Name)
+    }
+}
+
+$tagCount  | New-WordCloud -Path ..\WeightLoss1.svg -FocusWord "WeightLoss" -Typeface "Adobe Devanagari"
